d25afdffe1ca6b43a7ace393371de62faa84fb1e
Julian Haldenby <j.haldenby@gmail.com>
2013-05-22 15:44:33 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Puppet Bootstrap
+
+# Usage 
+This will bootstrap a puppet installation on centos, Ubuntu, or MACOS.
+
+To set up puppet on any of the supported platforms:
+
+`sudo install.sh`
+
+# Versions 
+
+On Mac facter-1.6.3 will be loaded, hiera-1.2.0, and puppet-3.2.1 will be installed.
+
+# License
+
+facter, heira and puppet are all licensed under apache 2.0 and are owned by Puppet Labs Inc. 
+Please see LICENSE.
---
+++ install.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+if [ `id -u` -ne 0 ]; then
+    echo "Must run install as root, try sudo $0"
+    exit 1
+fi
+
+if [ -f "/etc/lsb-release" ]; then
+	. /etc/lsb-release
+	wget http://apt.puppetlabs.com/puppetlabs-release-${DISTRIB_CODENAME}.deb
+	sudo dpkg -i puppetlabs-release-${DISTRIB_CODENAME}.deb
+	sudo apt-get update
+	sudo apt-get install puppet -y
+elif [ -f "/etc/redhat-release" ]; then
+	rpm -ivh http://yum.puppetlabs.com/el/6/products/i386/puppetlabs-release-6-7.noarch.rpm
+	yum install puppet -y
+elif [ `uname -s` = "Darwin" ]; then 
+	pushd '/usr/local'
+		mkdir homebrew && curl -L https://github.com/mxcl/homebrew/tarball/master | tar xz --strip 1 -C homebrew
+	popd
+	ln -s /usr/local/bin/brew /usr/local/bin/brew
+	sudo -S installer -verbose -pkg facter-1.6.3.pkg -target "/Volumes/Macintosh HD"
+	sudo -S installer -verbose -pkg hiera-1.2.0-rc1.pkg -target "/Volumes/Macintosh HD"
+	sudo -S installer -verbose -pkg hiera-puppet-1.0.0.pkg -target "/Volumes/Macintosh HD"
+	sudo -S installer -verbose -pkg puppet-3.2.1-rc1.pkg -target "/Volumes/Macintosh HD"
+fi
